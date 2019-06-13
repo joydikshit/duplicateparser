6d6fc78f82c012a0469d12898908b63bfaeda364
Joy R Dikshit <joydixit@gmail.com>
2019-06-13 18:36:36 -0400

    Initial set up of Project

---
+++ src/main/java/com/joydikshit/duplicateparser/DuplicateparserApplication.java
@@ -0,0 +1,13 @@
+package com.joydikshit.duplicateparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicateparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuplicateparserApplication.class, args);
+	}
+
+}
