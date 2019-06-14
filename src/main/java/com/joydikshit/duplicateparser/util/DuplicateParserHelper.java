ef7b9f9f3c3882cb21b4108de436e1700e0fa3bf
Joy R Dikshit <joydixit@gmail.com>
2019-06-13 21:25:57 -0400

    Have the Advanced file working as well now. Both duplicates and nonduplicates endpoints for normal and advanced working

--- src/main/java/com/joydikshit/duplicateparser/util/DuplicateParserHelper.java
+++ src/main/java/com/joydikshit/duplicateparser/util/DuplicateParserHelper.java
@@ -48,18 +48,49 @@ public class DuplicateParserHelper {
         return duplicates;
     }
 
-    private static void buildDuplicateSet(List<Person> allrecords) {
-        for (Person person : allrecords) {
-            if (emailMap.containsKey(person.getEmail())) {
+    private static void buildDuplicateSet(List<Person> allRecords) {
+        for (Person person : allRecords) {
+            String email = person.getEmail();
+            String phone = person.getPhoneNumber();
+            if (emailMap.containsKey(email)) {
                 //make sure that the other few fields also closely match
-                Person existingPerson = emailMap.get(person.getEmail()).stream().findFirst().get();
+                Person existingPerson = emailMap.get(email).stream().findFirst().get();
                 if (isAlmostMatchAfterEmailCheck(existingPerson, person)) {
-                    emailMap.get(person.getEmail()).add(person);
+                    //also lets add it to the phoneMap
+                    if (phoneMap.containsKey(existingPerson.getPhoneNumber())) {
+                        phoneMap.get(existingPerson.getPhoneNumber()).add(person);
+                    } else if (existingPerson.getPhoneNumber().length() > 0) {
+                        //phone number is different and is non null let's add it to phone Map, emails taken care of
+                        List<Person> personList = new ArrayList<>();
+                        personList.add(person);
+                        phoneMap.put(existingPerson.getPhoneNumber(), personList);
+                    }
+                    emailMap.get(email).add(person);
                 }
             } else {
-                List<Person> personList = new ArrayList<>();
-                personList.add(person);
-                emailMap.put(person.getEmail(), personList);
+                //lets check the phone number from the phone Map
+                if (phoneMap.containsKey(phone)) {
+                    Person existingPerson = phoneMap.get(phone).stream().findFirst().get();
+                    if (isAlmostMatchAfterPhoneCheck(existingPerson, person)) {
+                        //here the phone number exists in phone Map, email does not, but existing person email must exist
+                        //so let's update the email map as well
+                        emailMap.get(existingPerson.getEmail()).add(person);
+                        phoneMap.get(phone).add(person);
+                    }
+                } else {
+                    if (phone.length() > 0) {
+                        //email has not matched and phone also not matched, let's add it to phoneMap
+                        List<Person> personList = new ArrayList<>();
+                        personList.add(person);
+                        phoneMap.put(phone, personList);
+                    }
+                }
+                if (email.length() > 0) {
+                    List<Person> personList = new ArrayList<>();
+                    personList.add(person);
+                    emailMap.put(email, personList);
+                }
+
             }
         }
     }
@@ -76,6 +107,34 @@ public class DuplicateParserHelper {
 
         //The phone number matches too, let's just check either of first name and last name and return true
         if (phoneNoMatched && (firstNameMatched || lastNameMatched)) {
+            return true;
+        }
+
+        //If both first name and last name matches (along with the exact match on email), then lets return true
+        if (firstNameMatched && lastNameMatched) {
+            return true;
+        }
+
+        //If the address and zip and state matches lets return true
+        if (addressMatched && zipMatched && stateCodeMatched) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isAlmostMatchAfterPhoneCheck(Person firstPerson, Person secondPerson) {
+        LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+
+        boolean emailMatched = levenshteinDistance.apply(firstPerson.getEmail(), secondPerson.getEmail()) < 1;
+        boolean firstNameMatched = levenshteinDistance.apply(firstPerson.getFirstName(), secondPerson.getFirstName()) < 3;
+        boolean lastNameMatched = levenshteinDistance.apply(firstPerson.getLastName(), secondPerson.getLastName()) < 2;
+        boolean addressMatched = levenshteinDistance.apply(firstPerson.getAddress1(), secondPerson.getAddress1()) < 3;
+        boolean zipMatched = levenshteinDistance.apply(firstPerson.getZip(), secondPerson.getZip()) < 1;
+        boolean stateCodeMatched = levenshteinDistance.apply(firstPerson.getStateCode(), secondPerson.getStateCode()) < 1;
+
+        //The email matches too, let's just check either of first name and last name and return true
+        if (emailMatched && (firstNameMatched || lastNameMatched)) {
             return true;
         }
 
